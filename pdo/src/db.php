d210b6e2d7f0427d47f340a8d4584a969df9ed52
yuriy-kuzin-webdev <yuriy.kuzin.webdev@gmail.com>
2024-09-12 09:50:29 +0200

    feat: pdo/db.php added

---
+++ pdo/src/db.php
@@ -0,0 +1,14 @@
+<?php
+
+$host = 'localhost';
+$db = 'library';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    die("Помилка підключення до бази даних: " . $e->getMessage());
+}
+?>
